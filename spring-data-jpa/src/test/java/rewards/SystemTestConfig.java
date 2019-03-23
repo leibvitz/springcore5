011a703ded1e8ef63355098dc83692934d3d7419
Alex Garcia <leibvitzssariews@gmail.com>
2019-03-23 21:08:08 +0100

    SPRING DATA JPA - Solution

--- spring-data-jpa/src/test/java/rewards/SystemTestConfig.java
+++ spring-data-jpa/src/test/java/rewards/SystemTestConfig.java
@@ -1,19 +1,9 @@
 package rewards;
 
-import java.util.Properties;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import config.RewardsConfig;
 
@@ -31,54 +21,10 @@ import config.RewardsConfig;
 //
 // TODO-14: Run the test. The test still should pass.
 @Configuration
+@EnableJpaRepositories("rewards")
+@EnableAutoConfiguration
 @Import(RewardsConfig.class)
 public class SystemTestConfig {
 	
-	/**
-	 * Creates an in-memory "rewards" database populated 
-	 * with test data for fast testing
-	 */
-	@Bean
-	public DataSource dataSource(){
-		return
-			(new EmbeddedDatabaseBuilder())
-			.addScript("classpath:rewards/testdb/schema.sql")
-			.addScript("classpath:rewards/testdb/data.sql")
-			.build();
-	}	
-	
-	
-	/**	
-	 * LocalContainerEntityManagerFactoryBean. 
-	 */
-	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
-		
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setShowSql(true);
-		adapter.setGenerateDdl(true);
-		adapter.setDatabase(Database.HSQL);
-
-		Properties props = new Properties();
-		props.setProperty("hibernate.format_sql", "true");
-		
-		LocalContainerEntityManagerFactoryBean emfb = 
-			new LocalContainerEntityManagerFactoryBean();
-		emfb.setDataSource(dataSource());
-		emfb.setPackagesToScan("rewards.internal");
-		emfb.setJpaProperties(props);
-		emfb.setJpaVendorAdapter(adapter);
-		
-		return emfb;
-	}
-
-
-	/**	
-	 * JpaTransactionManager. 
-	 */
-	@Bean
-	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-		return new JpaTransactionManager(emf);
-	}
 		
 }
